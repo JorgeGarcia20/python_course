7be96deabf7a6f0e2cf0bc07ae216c147b3cc941
JorgeGarcia20 <alberto-garcia20@outlook.com>
2020-04-18 20:52:56 -0500

    Nuevo tema, creación de clases

---
+++ 20Clases.py
@@ -0,0 +1,15 @@
+class lapiz:
+    """
+    este es un ejemplo de clases en python, creacion de clase y definicion de 
+    atributos, creacion de objeto e impresion.
+    """
+    color = 'amarillo'
+    tiene_borrador = False
+    tiene_goma = True
+
+lapiz_generico = lapiz()
+
+print(lapiz_generico.color)
+print(lapiz_generico.tiene_borrador)
+print(lapiz_generico.tiene_goma)
+
