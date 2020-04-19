a079fe0f82963595f4c60cc03d2708943343e3ac
JorgeGarcia20 <alberto-garcia20@outlook.com>
2020-04-19 16:44:48 -0500

    creacion de metodos entre ellos el metodo __init__

--- 20Clases.py
+++ 20Clases.py
@@ -3,13 +3,44 @@ class lapiz:
     este es un ejemplo de clases en python, creacion de clase y definicion de 
     atributos, creacion de objeto e impresion.
     """
-    color = 'amarillo'
-    tiene_borrador = False
-    tiene_goma = True
+    def __init__(self, color, tiene_goma, usa_grafito):
+        self.color = color
+        self.tiene_goma = tiene_goma
+        self.usa_grafito = usa_grafito
+    
 
-lapiz_generico = lapiz()
+    #METODOS
+    def dibujar(self):
+        print('El lapiz esta dibujando')
+    
+    def borrar(self):
+        """
+        este metodo recibe el parametro de la funcion puede_borrar
+        verifica que la condicion se cumpla y muestra un mensaje.
+        ejemplo de usar una funcion dentro de otra funcion.
+        """
+        if self.puede_borrar():
+            print('El lapiz esta borrando')
+        else:
+            print('El lapiz no tiene borrador')
+    
+    def puede_borrar(self):
+        """
+        esta funcion retorna el valor de la variable
+        tiene_borrador
+        """
+        return self.tiene_goma
+
+
+
+lapiz_generico = lapiz('Verde', True, True)
 
 print(lapiz_generico.color)
-print(lapiz_generico.tiene_borrador)
 print(lapiz_generico.tiene_goma)
+print(lapiz_generico.usa_grafito)
+
+print('---------------Ejecucion de metodos-------------')
+lapiz_generico.dibujar()
+#lapiz_generico.tiene_borrador = True
+lapiz_generico.borrar()
 
